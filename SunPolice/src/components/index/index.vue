98d3a51c549993d5e0fb682b8a0e3abb29967b93
xiongmaobeibei <510123948@qq.com>
2019-04-10 19:21:56 +0800

    add flex and rem

--- SunPolice/src/components/index/index.vue
+++ SunPolice/src/components/index/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <h1>首页</h1>
+  <div class="main-container">
+    <div class="all-container">
+      <div class="news-container">
+        <div class="main-head">
+          <span>>>>&nbsp;信息发布</span>
+        </div>
+        <div class="main-body">
+          <div class="news-report">
+            <div class="news-pic">
+
+            </div>
+            <div class="news-tab">
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="interact-container">
+        <div class="main-head">
+          <span>>>>&nbsp;警民互动</span>
+        </div>
+        <div class="main-body"></div>
+      </div>
+      <div class="feedback-container">
+        <div class="main-head">
+          <span>>>>&nbsp;联系反馈</span>
+        </div>
+        <div class="main-body"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +38,56 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+.main-container
+  background-color rgb(82,130,170)
+  height auto
+  width 100%
+  .all-container
+    width 85%
+    height auto
+    margin 0 auto
+    margin-top -20px
+    .news-container
+      width 100%
+      height auto
+      .news-report
+        width 100%
+        height auto
+        display flex
+        flex-direction row
+        justify-content space-between
+        background-color pink
+        .news-pic
+          width 48%
+          height 300px
+          background-color red
+        .news-tab
+          width 48%
+          margin-right 0
+          height 300px
+          background-color blue
+    .interact-container
+      width 100%
+      height auto
+    .feedback-container
+      width 100%
+      height auto
+  .main-head
+    height 74px
+    line-height 74px
+    width 100%
+    margin 0 0
+    background-image url(main-head.png)
+    background-repeat no-repeat
+    background-size 100% 100%
+    span
+      color #99FFFF
+      font-size 30px
+      font-weight 800
+      padding-left 50px
+  .main-body
+    width 98%
+    height auto
+    background-color white
+    padding 20px 1%
 </style>
--- SunPolice/src/main.js
+++ SunPolice/src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import 'lib-flexible'
 import VueRouter from 'vue-router'
 import log from './pages/log/log'
 import login from './pages/log/login'
