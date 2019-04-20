998e75c8c0f714ba4193644b536121fbac9d6bd4
aiuer <tuyaobb@126.com>
2019-04-20 13:30:16 +0800

    regist

--- SunPolice/src/pages/log/login.vue
+++ SunPolice/src/pages/log/login.vue
@@ -2,9 +2,9 @@
     <div class="wrapper">
         <form action="GET" class="form">
             <p v-show="showtip">{{showtip}}</p>
-            <input type="text" v-model="creditid"  class="inputtext"/><br>
-            <input type="password" v-model="password" value="请输入密码" class="inputtext"/><br>
-            <input type="button" value="登录" width="30px" class="button" @click="login">
+            <input type="text" v-model="creditid" placeholder="   请输入身份证号码"  class="inputtext"/><br>
+            <input type="password" v-model="password" placeholder="   请输入密码" class="inputtext"/><br>
+            <input type="button" value="登录" class="button" @click="login">
         </form>
     </div>
 </template>
@@ -57,22 +57,26 @@ export default {
 <style lang="stylus" scoped rel="stylesheet/stylus">
   .wrapper
     background rgb(82,130,170)
-    height 600px
+    height 700px
     .form
       width 50%
-      height 300px
+      height 60%
       margin 150px auto 150px auto
-      padding-top 50px
+      padding-top 60px
       background white
       border-radius 30px
       text-align center
-      line-height 40px
+      line-height 80px
       font-size 18px
+      -webkit-box-shadow 14px 12px 16px #333333
+      -moz-box-shadow 13px 12px 16px #333333
       .inputtext
-        height 15px
-        width 180px
-      .button
         height 40px
+        width 60%
+      .button
+        height 50px
+        width 60%
         font-size 20px
+        color blue
         border 0px
 </style>
--- SunPolice/src/pages/log/regist.vue
+++ SunPolice/src/pages/log/regist.vue
@@ -1,5 +1,22 @@
 <template>
-    <div></div>
+    <div>
+        <div class="wrapper">
+            <form action="" class="formbox">
+                <span class="title">姓名</span><input class="input-box" type="text"><br/>
+                <span class="title">身份证号</span><input class="input-box" type="text"><br/>
+                <span class="title">密码</span><input class="input-box" type="password"><br/>
+                <span class="title">确认密码</span><input class="input-box" type="password"><br/>
+                <span class="title">性别</span>男：<input type="radio">&nbsp;女：<input type="radio"><br/>
+                <span class="title">民族</span><input class="input-box" type="text"><br/>
+                <span class="title">出生日期</span><input class="input-box" type="text"><br/>
+                <span class="title">电话号码</span><input class="input-box" type="text"><br/>
+                <span class="title">备用号码</span><input class="input-box" type="text"><br/>
+                <span class="title">电子邮箱</span><input class="input-box" type="text"><br/>
+                <span class="title">常住地址</span><input class="input-box" type="text"><br/>
+                <input class="button" type="button" value="注册">
+            </form>
+        </div>
+    </div>
 </template>
 <script>
 export default {
@@ -7,4 +24,26 @@ export default {
 </script>
 
 <style lang="stylus" scoped rel="stylesheet/stylus">
+.wrapper
+  background white
+  width 80%
+  height 900px
+  margin 80px auto 20px auto
+  .formbox
+    padding-top 80px
+    width 60%
+    font-size 24px
+    line-height 60px
+    margin auto
+    .title
+        display inline-block
+        width 30%
+    .input-box
+        width 60%
+    .button
+        width 80%
+        margin-left 5%
+        font-size 30px
+        background blue
+        border 0
 </style>
